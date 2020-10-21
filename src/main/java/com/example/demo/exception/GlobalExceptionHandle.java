93134d3f69085ec31ec9bff29b4fcf953455cd59
annie <anqi.chen@thoughtworks.com>
2020-10-21 20:10:58 +0800

    [anqi] feat error

--- src/main/java/com/example/demo/exception/GlobalExceptionHandle.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandle.java
@@ -1,16 +1,20 @@
 package com.example.demo.exception;
 
+import com.example.demo.dto.Error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Date;
+
 @RestControllerAdvice
 public class GlobalExceptionHandle {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity methodArgumentHandle(MethodArgumentNotValidException ex) {
         String message = ex.getBindingResult().getFieldError().getDefaultMessage();
+        Error error = new Error(new Date().toString(), HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), message);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
     }
 
