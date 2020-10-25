9f45f1057978052859564614dc914df236112207
annie <anqi.chen@thoughtworks.com>
2020-10-25 16:16:43 +0800

    fix tables init

--- src/main/java/com/example/demo/service/EducationService.java
+++ src/main/java/com/example/demo/service/EducationService.java
@@ -20,15 +20,18 @@ public class EducationService {
     public EducationService(EducationRepository educationRepository, UserRepository userRepository){
         this.educationRepository = educationRepository;
         this.userRepository = userRepository;
-        User user = userRepository.findById(1L).get();
-        Education edu1 =  Education.builder().user(user).year(2005).title("Secondary school specializing in artistic")
-                .description("Eos, explicabo, nam, tenetur et ab eius deserunt aspernatur ipsum ducimus quibusdam quis voluptatibus.")
-                .build();
-        educationRepository.save(edu1);
-        Education edu2 = Education.builder().user(user).year(2009).title("First level graduation in Graphic Design")
-                .description("Aspernatur, mollitia, quos maxime eius suscipit sed beatae ducimus quaerat quibusdam perferendis? Iusto, quibusdam asperiores unde repellat.")
-                .build();
-        educationRepository.save(edu2);
+//        Optional<User> user = userRepository.findById(1L);
+//        if(user.isPresent()){
+//            Education edu1 =  Education.builder().user(user.get()).year(2005).title("Secondary school specializing in artistic")
+//                    .description("Eos, explicabo, nam, tenetur et ab eius deserunt aspernatur ipsum ducimus quibusdam quis voluptatibus.")
+//                    .build();
+//            educationRepository.save(edu1);
+//            Education edu2 = Education.builder().user(user.get()).year(2009).title("First level graduation in Graphic Design")
+//                    .description("Aspernatur, mollitia, quos maxime eius suscipit sed beatae ducimus quaerat quibusdam perferendis? Iusto, quibusdam asperiores unde repellat.")
+//                    .build();
+//            educationRepository.save(edu2);
+//        }
+
     }
 
     public List<Education> getEducationById(long id){
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -17,10 +17,10 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        User user = User.builder().name("KAMIL").age(24).avatar("https://inews.gtimg.com/newsapp_match/0/3581582328/0")
-                .description("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, non, dolorem, cumque distinctio magni quam expedita velit laborum sunt amet facere tempora ut fuga aliquam ad asperiores voluptatem dolorum! Quasi.")
-                .build();
-        userRepository.save(user);
+//        User user = User.builder().name("KAMIL").age(24).avatar("https://inews.gtimg.com/newsapp_match/0/3581582328/0")
+//                .description("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, non, dolorem, cumque distinctio magni quam expedita velit laborum sunt amet facere tempora ut fuga aliquam ad asperiores voluptatem dolorum! Quasi.")
+//                .build();
+//        userRepository.save(user);
     }
 
     public User createUser(User user){
